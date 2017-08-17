818d90035b7922a3194f5279220b764d74e75bfb
ChrisYang <chrisyang@yunify.com>
2017-08-17 18:34:38 +0800

    Add ImageProcessor util class

---
+++ Sources/ImageProcessor.swift
@@ -0,0 +1,230 @@
+//
+//  QingStorAPIs.swift
+//
+// +-------------------------------------------------------------------------
+// | Copyright (C) 2017 Yunify, Inc.
+// +-------------------------------------------------------------------------
+// | Licensed under the Apache License, Version 2.0 (the "License");
+// | you may not use this work except in compliance with the License.
+// | You may obtain a copy of the License in the LICENSE file, or at:
+// |
+// | http://www.apache.org/licenses/LICENSE-2.0
+// |
+// | Unless required by applicable law or agreed to in writing, software
+// | distributed under the License is distributed on an "AS IS" BASIS,
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// | See the License for the specific language governing permissions and
+// | limitations under the License.
+// +-------------------------------------------------------------------------
+//
+
+import Foundation
+
+public protocol ImageProcessCodeable {
+    func processCode() -> String
+}
+
+fileprivate protocol Codeable {
+    func code() -> String
+}
+
+fileprivate protocol Formatable {
+    func format() -> String
+}
+
+extension Formatable {
+    func format() -> String {
+        return String(describing: self)
+    }
+}
+
+extension Int: Formatable { }
+extension Float: Formatable { }
+
+extension String: Formatable {
+    func format() -> String {
+        return self
+    }
+}
+
+public enum ImageProcess: ImageProcessCodeable {
+    case crop(width: Int?, height: Int?, gravity: CropGravity?)
+    case rotate(angle: Int)
+    case resize(width: Int?, height: Int?, mode: ResizeMode?)
+    case watermark(text: String, color: String?, opacity: Float?, dpi: Int?)
+    case watermarkImage(url: String, left: Int?, top: Int?, opacity: Float?)
+    case format(type: FormatType)
+    
+    fileprivate struct Code<T: Formatable>: Codeable {
+        let prefix: String
+        let value: T?
+        let base64: Bool
+        
+        var description: String {
+            if let value = value {
+                var format = value.format()
+                if base64 {
+                    // Convert to Base64 and make safe for URLs
+                    format = format.data(using: .utf8)!.base64EncodedString()
+                    format = format.replacingOccurrences(of: "/", with: "_")
+                    format = format.replacingOccurrences(of: "+", with: "-")
+                    format = format.trimmingCharacters(in: CharacterSet(charactersIn: "="))
+                }
+                
+                return "\(prefix)_\(format)"
+            }
+            
+            return ""
+        }
+        
+        init(prefix: String, value: T? = nil, base64: Bool = false) {
+            self.prefix = prefix
+            self.value = value
+            self.base64 = base64
+        }
+        
+        func code() -> String {
+            return self.description
+        }
+    }
+    
+    public func processCode() -> String {
+        switch self {
+            
+        case let .crop(width, height, gravity):
+            if width == nil && height == nil {
+                return ""
+            }
+            
+            let code = formatCodes([Code(prefix: "w", value: width),
+                                    Code(prefix: "h", value: height),
+                                    Code(prefix: "g", value: gravity)])
+            return "crop:\(code)"
+            
+        case let .rotate(angle):
+            let code = Code(prefix: "a", value: angle).description
+            return "rotate:\(code)"
+            
+        case let .resize(width, height, mode):
+            if width == nil && height == nil {
+                return ""
+            }
+            
+            let code = formatCodes([Code(prefix: "w", value: width),
+                                    Code(prefix: "h", value: height),
+                                    Code(prefix: "m", value: mode)])
+            return "resize:\(code)"
+            
+        case let .watermark(text, color, opacity, dpi):
+            let code = formatCodes([Code(prefix: "t", value: text, base64: true),
+                                    Code(prefix: "c", value: color, base64: true),
+                                    Code(prefix: "p", value: opacity),
+                                    Code(prefix: "d", value: dpi)])
+            return "watermark:\(code)"
+            
+        case let .watermarkImage(url, left, top, opacity):
+            let code = formatCodes([Code(prefix: "u", value: url, base64: true),
+                                    Code(prefix: "l", value: left),
+                                    Code(prefix: "t", value: top),
+                                    Code(prefix: "p", value: opacity)])
+            return "watermark_image:\(code)"
+            
+        case let .format(type):
+            let code = Code(prefix: "t", value: type).description
+            return "format:\(code)"
+        }
+    }
+    
+    fileprivate func formatCodes(_ codes: [Codeable]) -> String {
+        return codes
+            .map { $0.code() }
+            .filter { !$0.isEmpty }
+            .joined(separator: ",")
+    }
+}
+
+public enum CropGravity: Int, Formatable {
+    case center    = 0
+    case north     = 1
+    case east      = 2
+    case south     = 3
+    case west      = 4
+    case northWest = 5
+    case northEast = 6
+    case southWest = 7
+    case southEast = 8
+    case auto      = 9
+    
+    fileprivate func format() -> String {
+        return self.rawValue.format()
+    }
+}
+
+public enum ResizeMode: Int, Formatable {
+    case fixed     = 0
+    case force     = 1
+    case thumbnail = 2
+    
+    fileprivate func format() -> String {
+        return self.rawValue.format()
+    }
+}
+
+public enum FormatType: String, Formatable {
+    case jpeg = "jpeg"
+    case png  = "png"
+    case webp = "webp"
+    case tiff = "tiff"
+    
+    fileprivate func format() -> String {
+        return self.rawValue
+    }
+}
+
+public class ImageProcessor {
+    public var processList: [ImageProcessCodeable] = []
+    
+    public init(processList: [ImageProcessCodeable] = []) {
+        self.processList = processList
+    }
+    
+    public func process(_ process: ImageProcessCodeable) -> ImageProcessor {
+        processList.append(process)
+        return self
+    }
+    
+    public func crop(width: Int?, height: Int?, gravity: CropGravity?) -> ImageProcessor {
+        return process(ImageProcess.crop(width: width, height: height, gravity: gravity))
+    }
+    
+    public func rotate(angle: Int) -> ImageProcessor {
+        return process(ImageProcess.rotate(angle: angle))
+    }
+    
+    public func resize(width: Int?, height: Int?, mode: ResizeMode?) -> ImageProcessor {
+        return process(ImageProcess.resize(width: width, height: height, mode: mode))
+    }
+    
+    public func watermark(text: String, color: String?, opacity: Float?, dpi: Int?) -> ImageProcessor {
+        return process(ImageProcess.watermark(text: text, color: color, opacity: opacity, dpi: dpi))
+    }
+    
+    public func watermarkImage(url: String, left: Int?, top: Int?, opacity: Float?) -> ImageProcessor {
+        return process(ImageProcess.watermarkImage(url: url, left: left, top: top, opacity: opacity))
+    }
+    
+    public func format(type: FormatType) -> ImageProcessor {
+        return process(ImageProcess.format(type: type))
+    }
+    
+    public func resetProcessing() {
+        processList.removeAll()
+    }
+    
+    public func processingResult() -> String {
+        return processList
+            .map { $0.processCode() }
+            .filter { !$0.isEmpty }
+            .joined(separator: "|")
+    }
+}
---
+++ Tests/QingStorSDKTests/ImageProcessorTests.swift
@@ -0,0 +1,114 @@
+//
+//  QingStorAPIs.swift
+//
+// +-------------------------------------------------------------------------
+// | Copyright (C) 2017 Yunify, Inc.
+// +-------------------------------------------------------------------------
+// | Licensed under the Apache License, Version 2.0 (the "License");
+// | you may not use this work except in compliance with the License.
+// | You may obtain a copy of the License in the LICENSE file, or at:
+// |
+// | http://www.apache.org/licenses/LICENSE-2.0
+// |
+// | Unless required by applicable law or agreed to in writing, software
+// | distributed under the License is distributed on an "AS IS" BASIS,
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// | See the License for the specific language governing permissions and
+// | limitations under the License.
+// +-------------------------------------------------------------------------
+//
+
+import XCTest
+@testable import QingStorSDK
+
+class ImageProcessorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testCrop() {
+        let result = "crop:w_100,h_200,g_6"
+        let target = ImageProcessor()
+            .crop(width: 100, height: 200, gravity: .northEast)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+    
+    func testRotate() {
+        let result = "rotate:a_360"
+        let target = ImageProcessor()
+            .rotate(angle: 360)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+    
+    func testResize() {
+        let result = "resize:w_200,h_400,m_2"
+        let target = ImageProcessor()
+            .resize(width: 200, height: 400, mode: .thumbnail)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+    
+    func testWatermark() {
+        let result = "watermark:t_5rC05Y2w5rWL6K-V,c_I2ZmMDBmZg,p_0.1123,d_250"
+        let target = ImageProcessor()
+            .watermark(text: "水印测试", color: "#ff00ff", opacity: 0.1123, dpi: 250)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+    
+    func testWatermarkImage() {
+        let result = "watermark_image:u_aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc,l_50,t_100,p_1.0"
+        let target = ImageProcessor()
+            .watermarkImage(url: "https://pek3a.qingstor.com/img-doc-eg/qincloud.png", left: 50, top: 100, opacity: 1.000)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+    
+    func testFormat() {
+        let result = "format:t_tiff"
+        let target = ImageProcessor()
+            .format(type: .tiff)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+
+    func testCombinationProcess() {
+        let result = "rotate:a_180|watermark_image:u_aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc,l_20,t_80,p_0.1|crop:w_850|watermark:t_5paH5pysdGV4dA|resize:h_1024,m_1|format:t_png"
+        let target = ImageProcessor()
+            .rotate(angle: 180)
+            .resize(width: nil, height: nil, mode: nil)  // invalid operate, will ignore
+            .watermarkImage(url: "https://pek3a.qingstor.com/img-doc-eg/qincloud.png", left: 20, top: 80, opacity: 0.1)
+            .crop(width: 850, height: nil, gravity: nil)
+            .watermark(text: "文本text", color: nil, opacity: nil, dpi: nil)
+            .resize(width: nil, height: 1024, mode: .force)
+            .crop(width: nil, height: nil, gravity: nil) // invalid operate, will ignore
+            .format(type: .png)
+            .processingResult()
+        
+        XCTAssertEqual(target, result)
+    }
+    
+    func testResetProcessing() {
+        let imageProcessor = ImageProcessor()
+        
+        let result = "rotate:a_180"
+        let target = imageProcessor.rotate(angle: 180).processingResult()
+        XCTAssertEqual(target, result)
+        
+        imageProcessor.resetProcessing()
+        XCTAssertEqual("", imageProcessor.processingResult())
+    }
+}
