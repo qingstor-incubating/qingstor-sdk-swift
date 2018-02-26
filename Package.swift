2779e41685d06d964345397a456fd0d3f075a628
ChrisYang <chrisyang@yunify.com>
2018-02-26 10:34:46 +0800

    Update dependency Library

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "QingStorSDK",
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4, minor: 5),
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4, minor: 6),
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 3, minor: 1),
         .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0, minor: 7)
         ],
